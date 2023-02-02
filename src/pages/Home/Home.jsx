1505b68b009fbaa5dfa5ce69e34c79d750a7df31
bakisultanov@gmail.com <bakisultanov@gmail.com>
2023-02-02 18:06:03 +0300

    dobavil stiley

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,57 +1,30 @@
-import React, { useState } from 'react'
-import TodoItem from '../../components/TodoItem'
-import TodoForm from '../../components/TodoForm'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 
 const Home = () => {
   const [todos, setToDos] = useState([])
-
-  const addToDo = (userInput) => {
-    if(userInput) {
-      const newToDo = {
-        id: Math.random().toString(36).substr(2,9),
-        text: userInput,
-        completed: false
-      }
-      setToDos([...todos, newToDo])
-    }
-  }
-
-  const removeTodo = (id) => {
-    setToDos([...todos.filter((todo) => todo.id !== id)])
-  }
-
-  const handleToggle = (id) => {
-    setToDos([...todos.map((todo) => todo.id === id ? {...todo, complete: !todo.completed} : {todo})])
-  }
-
+  const [text, setText] = useState('')
+  useEffect(()=>{
+    console.log(todos);
+  })
   return (
     <div className="wrapper">
       <main className='home__block'>
         <div className='container'>
-          <div className="home__inner">
-            <h1>To do list (Team work)</h1>
-            <br/>
-            <TodoForm 
-            addToDo={addToDo}
-            />
-            <ul className='todo__list'>
-              {
-                todos.map((todo) => {
-                  return (
-                  <TodoItem
-                  todo={todo}
-                  key={todo.id}
-                  removeTodo={removeTodo}
-                  toggleToDo={handleToggle}
-                  />
-                  )
-
-                })
-              }
-            </ul>
-          </div>
-          <p>{todos.length}</p>
+          <form className='todo__form' onSubmit={e=>{
+            e.preventDefault()
+            setToDos([...todos, {text}])
+          }}>
+            <input type="text" value={text} onChange={e=>setText(e.target.value)} />
+            <button>Add</button>
+          </form>
+          <ul className='todo__list'>
+            {
+            todos.map(todo => (
+              <li className='todo__item' key={todo.id}>{todo.text}</li>
+            ))
+            }
+          </ul>
         </div>
       </main>
     </div>
