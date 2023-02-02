21b7b22fe617d5ab3db1701951cde443ef545c8b
jodar-Nimatov <115215766+jodar-Nimatov@users.noreply.github.com>
2023-02-02 18:02:03 +0600

    Made counter

--- src/pages/Counter/Counter.jsx
+++ src/pages/Counter/Counter.jsx
@@ -2,12 +2,31 @@ import React, { useState } from 'react'
 import './Counter.css'
 
 const Counter = () => {
-    const [count, setCount] = useState(1)
+    const [count, setCount] = useState(0)
+
+    const initialState = 0
+
+
+    const increment = () => {
+        setCount(count + 1)
+    }
+
+    const decrement = () => {
+        setCount(count - 1)
+    }
+
+    const reset = () => {
+        setCount(() => initialState)
+    }
+
     return (
         <div className="counter__block">
             <div className='container'>
                 <div className="counter__inner">
-
+                    <h1>{count}</h1>
+                    <button onClick={increment}>+</button>
+                    <button onClick={decrement}>-</button>
+                    <button onClick={reset}>reset</button>
                 </div>
             </div>
         </div>
