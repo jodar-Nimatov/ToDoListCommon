4fa6a8499b61ac4073b7dd838fa7559c23fb8468
bakisultanov@gmail.com <bakisultanov@gmail.com>
2023-02-02 19:04:02 +0300

    btns

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,25 +4,38 @@ import './Home.css'
 const Home = () => {
   const [todos, setToDos] = useState([])
   const [text, setText] = useState('')
-  useEffect(()=>{
-    console.log(todos);
-  })
+  // Незнаю как но это надо использовать
+  // useEffect(()=>{
+  //   console.log(todos);
+  // }, [])
   return (
     <div className="wrapper">
       <main className='home__block'>
         <div className='container'>
           <form className='todo__form' onSubmit={e=>{
             e.preventDefault()
-            setToDos([...todos, {text}])
+            if(text.trim().length){
+              setText('')
+              setToDos([...todos, {text}])
+            }
           }}>
             <input type="text" value={text} onChange={e=>setText(e.target.value)} />
             <button>Add</button>
           </form>
           <ul className='todo__list'>
             {
-            todos.map(todo => (
-              <li className='todo__item' key={todo.id}>{todo.text}</li>
-            ))
+            todos.map(todo => {
+                return (
+                  <div className='todoshka'>
+                    <li className='todo__item' key={todo.id}>{todo.text}</li>
+                    <div className="knopki">
+                      <button className='del'>delete</button>
+                      <button className='pen'>🖊️</button>
+                    </div>
+                  </div>
+                
+                )
+            })
             }
           </ul>
         </div>
